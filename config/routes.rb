2376666031f80ad8a4fb4a399bc8d1bbcfbfe79d
divine13 <dubedivine@gmail.com>
2013-10-19 15:33:59 +0200

    done with all user and cancan logic not tested

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Zap::Application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
+  devise_for :users
+  
+
    root 'static_pages#index'
 
+
+   namespace :admin do
+      resources :users
+    end
+    
+    resources :posts do 
+      resources :comments
+    end
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
