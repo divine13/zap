01083b54aa7b5c00ce604660a582888e1d6830cd
divine13 <dubedivine@gmail.com>
2013-10-20 19:00:19 +0200

    finished with posts and comments you can now post and comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,22 @@
 class CommentsController < ApplicationController
+	before_filter :authenticate_user!
+	before_filter :find_post
 	def create 
-
+		@comment = @post.comments.build(comment_params)
+		if @comment.save 
+			flash[:success] = "thank you for commenting"
+			redirect_to([ @post]) #change to comment if it does not work
+		else 
+			flash[:error] = "sorry could not comment"
+			render template: 'posts/show'
+			#render @post
+		end
 	end
 	def comment_params
 		prms = params.require(:comment).permit!
+		prms.merge!(user: current_user)
 	end
 	def find_post
-		@post = Post.find(params([:post_id]))
+		@post = Post.find(params[:post_id])
 	end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,11 @@
 class Post < ActiveRecord::Base
   belongs_to :user
-
+  has_many :comments
   has_attached_file :picture
 
   validates(:about, presence: true, length: { minimum: 10 })
   validates(:contact_details, presence: true, length: {minimum: 3 } )
   validates(:designer, presence: true )
 
-  has_many :comments
+
 end
